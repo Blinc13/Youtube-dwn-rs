80c658e4eb61ccd60be9c5f3ca94845537259334
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-10-10 15:07:27 +0300

    Remove unused code

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -17,21 +17,6 @@ pub struct VideoMeta<'a> {
     pub views_count: usize
 }
 
-impl<'a> VideoMeta<'a> {
-    pub fn new(name: &'a str, description: &'a str, author: &'a str, keywords: Vec<&'a str>, id: &'a str, duration: usize, views_count: usize) -> Self {
-        Self {
-            name,
-            description,
-            author,
-            keywords,
-            id,
-            duration,
-            views_count
-        }
-    }
-}
-
-
 #[derive(Debug, Builder)]
 #[builder(setter(into))]
 pub struct Format<'a> {
@@ -47,20 +32,4 @@ pub struct Format<'a> {
     pub fps: usize,
 
     pub size: (u16, u16)
-}
-
-impl<'a> Format<'a> {
-    pub fn new(r#type: &'a str, file_format: &'a str, url: &'a str, quality: &'a str, quality_on_page_label: &'a str, duration_ms: usize, bitrate: usize, fps: usize, size: (u16, u16)) -> Self {
-        Self {
-            r#type,
-            file_format,
-            url,
-            quality,
-            quality_on_page_label,
-            duration_ms,
-            bitrate,
-            fps,
-            size
-        }
-    }
 }
